15570c07cec36a20ad72726bd3da252781208db7
Erobern <erobern@chino-2.local>
2014-05-11 13:57:07 -0300

    chagin fb share

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=141188425906733&version=v2.0";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 	<div class="navbar">
@@ -44,8 +44,7 @@
 	    <div class="container">
                     <img src="./logo.png" style="float:left;margin-top:25px; margin-right:10px;"/>
                     <a class="brand" href="http://triviacrackgenerator.com">
-                    
-        			Trivia Crack <br/>Generator
+                     Trivia Crack <br/>Generator
         			</a>
     <div style="float:right;margin-top:10px;">
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
--- meme.php
+++ meme.php
@@ -27,12 +27,11 @@
 
 <body>
     <!--Facebook share-->
-    <div id="fb-root"></div>
     <script>(function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=141188425906733&version=v2.0";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 	<div class="navbar">
