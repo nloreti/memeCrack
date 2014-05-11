cd7d5dcd77612faef4498c85d3a0f9dac0be8d8b
Erobern <erobern@chino-2.local>
2014-05-11 15:03:22 -0300

    Merge branch 'master' of https://github.com/nloreti/memeCrack

--- index.php
+++ index.php
@@ -31,14 +31,14 @@
 
 <body>
     <!--Facebook share-->
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=141188425906733&version=v2.0";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=104070986315547&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 	<div class="navbar">
 	  <div class="navbar-inner">
 	    <div class="container">
