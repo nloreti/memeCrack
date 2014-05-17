1679c34618921b73c4ee4adb3f58d879fdc868be
Erobern <erobern@chino-2.local>
2014-05-17 19:41:05 -0300

    facebok share things

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<!-- Facebook Tags -->
+<meta name="viewport" content="width=device-width"/>
+<meta property="og:title" content="Trivia Crack Generator"/>
+<meta property="og:type" content="article"/>
+<meta property="og:image" content="http://triviacrackgenerator.com/logo.png"/>
+<meta property="og:url" content="http://triviacrackgenerator.com" />
+<meta property="og:description" content="Creá tu propia pregunta graciosa de trivia crack" />
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 <script type='text/javascript' src='http://www.google.com/jsapi'></script>
 <script type="text/javascript" src="./bootstrap/js/jquery.js"></script>
@@ -18,20 +25,11 @@
 
 <title>Trivia Crack Generator</title>
 
-<!-- Facebook Tags -->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta name="viewport" content="width=device-width"/>
-<meta property="og:image" content="http://triviacrackgenerator.com/logo.png"/>
-<meta property="og:title" content="Trivia Crack Generator"/>
-<meta property="og:type" content="article"/>
-<meta property="og:description" content="Generate your own trivia crack meme" />
-<meta property="og:site_name" content="Trivia Crack Generator"/>
-<meta property="og:image" content="http://triviacrackgenerator.com/logo.png"/>
+
 
 <script type="text/javascript">
 $(document).ready(function() {
     $('#questionForm').bootstrapValidator();
-    
 </script>
 
 <script type="text/javascript">
